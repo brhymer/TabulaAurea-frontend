0680136e69a126e315a6856aea316a74579828be
Brhymer <leiqien28@hotmail.com>
2020-06-04 15:20:45 -0700

    adds config/routes

---
+++ src/components/Expenses.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Expenses extends Component {
+    render() {
+        return (
+            <div>
+                Expenses
+            </div>
+        );
+    }
+}
+
+export default Expenses;
