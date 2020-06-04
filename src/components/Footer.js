aed6a9cfaf413c5a03fa899a3968a194cbca670f
Brhymer <leiqien28@hotmail.com>
2020-06-04 15:17:38 -0700

    adds Header and Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <hr/>
+            &copy; 2020 getones 
+        </footer>
+    );
+}
+
+export default Footer;
