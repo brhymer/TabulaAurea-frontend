caf1c0c49fc5cf51125899e2f592b70083369a99
Brhymer <leiqien28@hotmail.com>
2020-06-08 14:48:15 -0700

    fleshes out asset forms

--- src/forms/AssetForm.js
+++ src/forms/AssetForm.js
@@ -1,10 +1,70 @@
 import React, { Component } from 'react';
 
 class AssetForm extends Component {
+    state = {
+        name: '',
+        value: '',
+        intRate: ''
+    }
+
+    onChange = (event) => {
+        this.setState({
+            name: event.target.value,
+        });
+    };
+
+    onChange2 = (event) => {
+        this.setState({
+            value: event.target.value,
+        });
+    };
+
+    onChange3 = (event) => {
+        this.setState({
+            intRate: event.target.value,
+        });
+    };
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.props.toggleBodyForm()
+        const asset = this.state;
+        this.props.updateAsset(asset, this.props.asset._id)
+    };
+
+    componentDidMount(){
+        this.setState({
+            name: this.props.asset.name, 
+            value: this.props.asset.value, 
+            intRate: this.props.asset.intRate
+        })
+    };
+
+
     render() {
         return (
-            <div>
-                
+            <div style={this.props.style} className="assetform">
+                <form onSubmit={ this.onSubmit }>
+                    <input
+                        autoFocus={ this.props.autoFocus }
+                        onChange={ this.onChange }
+                        placeholder={ this.props.name }
+                        type="text"
+                        value={(this.state.name) || ''}/>
+                    <input
+                        autoFocus={ this.props.autoFocus }
+                        onChange={ this.onChange }
+                        placeholder={ this.props.value }
+                        type="text"
+                        value={(this.state.value) || ''}/>
+                    <input
+                        autoFocus={ this.props.autoFocus }
+                        onChange={ this.onChange }
+                        placeholder={ this.props.intRate }
+                        type="text"
+                        value={(this.state.intRate) || ''}/>
+                    <button type= "submit" className="btn">{this.props.buttonName}</button>
+                </form>                
             </div>
         );
     }
--- src/forms/CreateExpenseForm.js
+++ src/forms/CreateExpenseForm.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 
 
 class CreateExpenseForm extends Component {
-    state ={
+    state = {
         name: '',
         amount: '',
     }
